3f564e5ab2e6c3c470c0127382e6437836787d95
joe <joe@replicate.com>
2024-08-16 15:43:53 +0000

    Make noise level `t` shape compatable with bs>1
    Currently, shape errors are thrown for bs>1 during noise injection. This commit adds dimensions for broadcast compatability and then removes them.

--- train_flux_lora_deepspeed.py
+++ train_flux_lora_deepspeed.py
@@ -221,9 +221,10 @@ def main():
 
                 bs = img.shape[0]
                 t = torch.sigmoid(torch.randn((bs,), device=accelerator.device))
-
+                t = t.unsqueeze(1).unsqueeze(2)
                 x_0 = torch.randn_like(x_1).to(accelerator.device)
                 x_t = (1 - t) * x_1 + t * x_0
+                t = t.squeeze()
                 bsz = x_1.shape[0]
                 guidance_vec = torch.full((x_t.shape[0],), 4, device=x_t.device, dtype=x_t.dtype)
 
